066f51382122d82138cfbd833679190c6b165259
yogesh <yogeshwhetwar@gmail.com>
2024-03-04 14:33:25 +0530

    2

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ require('dotenv').config()
 app.get('/',(req,res)=>{
 res.send("hello world");
 }
-)  
+)    
+const gitdata=[{
+    "A":"k",
+    "A":"k",
+    "A":"k",
+    "A":"k",
+    "A":"k",
+    "A":"k",
+    "A":"k"
+}]
 app.get('/twitter',(req,res)=>{
 res.send("twitter hello2 world");
+}
+)  
+app.get('/yogi',(req,res)=>{
+res.json(gitdata);
 }
 )  
 
